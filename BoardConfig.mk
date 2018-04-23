7370ef65b1a63b40cb40104630ee1e557b473b45
Ali Hasan <admin@ialihasan.com>
2018-04-23 12:29:55 +0000

    Bring Up BlissROM

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -235,6 +235,7 @@ BOARD_WPA_SUPPLICANT_PRIVATE_LIB := lib_driver_cmd_qcwcn
 WIFI_DRIVER_FW_PATH_AP := "ap"
 WIFI_DRIVER_FW_PATH_STA := "sta"
 WPA_SUPPLICANT_VERSION := VER_0_8_X
-
+BOARD_NO_CHARGER_LED := true
+WITH_BLISS_CHARGER := false
 # Inherit from the proprietary version
 -include vendor/xiaomi/mido/BoardConfigVendor.mk
